417ae62dd9233887b37621088ffbf155d2d851fc
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-03-17 19:36:33 +0300

    redux-import

--- todo-project/src/components/TodoCreate.tsx
+++ todo-project/src/components/TodoCreate.tsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createTodo } from "../redux/todoSlice";
+import { TodoType } from "../types/Types";
+
 function TodoCreate() {
+  const dispatch = useDispatch();
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const handleTodoCreate = () => {
+    if (newTodo.trim() === "") {
+      alert("Please enter a valid todo");
+      return;
+    }
+    const paytload: TodoType = {
+      id: Math.floor(Math.random() * 99999),
+      content: newTodo,
+    };
+    dispatch(createTodo(paytload));
+    setNewTodo("");
+  };
   return (
     <div className="todo-create">
       <h2>Create a new todo</h2>
@@ -6,8 +26,14 @@ function TodoCreate() {
         className="todo-create-input"
         type="text"
         placeholder="Enter your Todo..."
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setNewTodo(e.target.value)
+        }
+        value={newTodo}
       />
-      <button className="todo-create-button">Create</button>
+      <button onClick={handleTodoCreate} className="todo-create-button">
+        Create
+      </button>
     </div>
   );
 }
