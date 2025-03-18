7a3c0daee025b6cb4cc2b5bb3a9923e5b80760a9
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-03-18 08:36:35 +0300

    remove-todo

--- todo-project/src/components/Todo.tsx
+++ todo-project/src/components/Todo.tsx
@@ -2,14 +2,23 @@ import { IoIosRemoveCircleOutline } from "react-icons/io";
 import { FaEdit } from "react-icons/fa";
 // import { FaCheck } from "react-icons/fa";
 import { todoProps } from "../types/Types";
+import { useDispatch } from "react-redux";
+import { removeTodoById } from "../redux/todoSlice";
 
 function Todo({ todoProps }: todoProps) {
   const { id, content } = todoProps;
+  const dispacth = useDispatch();
+  const handleRemoveTodo = () => {
+    dispacth(removeTodoById(id));
+  };
   return (
     <div className="todo">
       <div key={id}>{content}</div>
       <div className="todo-icons">
-        <IoIosRemoveCircleOutline className="todo-icon" />
+        <IoIosRemoveCircleOutline
+          className="todo-icon"
+          onClick={handleRemoveTodo}
+        />
         <FaEdit className="todo-icon" />
       </div>
     </div>
