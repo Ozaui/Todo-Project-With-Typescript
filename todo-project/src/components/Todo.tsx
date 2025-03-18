0ab8b0823bada1895cf03dbe46db68bd69044a42
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-03-18 09:00:30 +0300

    edit-todo

--- todo-project/src/components/Todo.tsx
+++ todo-project/src/components/Todo.tsx
@@ -1,25 +1,56 @@
 import { IoIosRemoveCircleOutline } from "react-icons/io";
 import { FaEdit } from "react-icons/fa";
-// import { FaCheck } from "react-icons/fa";
-import { todoProps } from "../types/Types";
+import { FaCheck } from "react-icons/fa";
+import { todoProps, TodoType } from "../types/Types";
 import { useDispatch } from "react-redux";
-import { removeTodoById } from "../redux/todoSlice";
+import { removeTodoById, updateTodo } from "../redux/todoSlice";
+import { useState } from "react";
 
 function Todo({ todoProps }: todoProps) {
   const { id, content } = todoProps;
+
   const dispacth = useDispatch();
+
   const handleRemoveTodo = () => {
     dispacth(removeTodoById(id));
   };
+
+  const handleUpdateTodo = () => {
+    const payload: TodoType = {
+      id: id,
+      content: newTodo,
+    };
+    dispacth(updateTodo(payload));
+    setEditable(false);
+  };
+
+  const [editable, setEditable] = useState<boolean>(false);
+  const [newTodo, setNewTodo] = useState<string>(content);
   return (
     <div className="todo">
-      <div key={id}>{content}</div>
+      <div>
+        {editable ? (
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setNewTodo(e.target.value)
+            }
+          />
+        ) : (
+          <div key={id}>{content}</div>
+        )}
+      </div>
       <div className="todo-icons">
         <IoIosRemoveCircleOutline
           className="todo-icon"
           onClick={handleRemoveTodo}
         />
-        <FaEdit className="todo-icon" />
+        {editable ? (
+          <FaCheck className="todo-icon" onClick={handleUpdateTodo} />
+        ) : (
+          <FaEdit className="todo-icon" onClick={() => setEditable(true)} />
+        )}
       </div>
     </div>
   );
